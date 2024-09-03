4cc6608e2eeb5809d545c582428b36b6f0aaf3e8
GGermanBoldyrev <boldyrev_german@mail.ru>
2024-09-03 21:08:18 +0300

    as file

--- as.php
+++ as.php
@@ -1,5 +1,45 @@
 <?php
 
+enum SenderTypes
+{
+    case Telegram;
+    case Email;
+}
+
+class Notify
+{
+    public int $id;
+    public int $userId;
+    public int $period;
+    public string $text;
+}
+
+interface NotifyCreator
+{
+    function create(int $userId, int $periodMinutes, string $text): int;
+}
+
+interface NotifySendersService
+{
+    /**
+     * @param DateTime $dateTime
+     * @return array<Notify>
+     */
+    function getNotSends(DateTime $dateTime): array;
+
+    function setNotificationSended(int $notifyId, SenderTypes $type);
+}
+
+interface SenderNotify
+{
+    function send(Notify $notify): bool;
+}
+
+interface SenderFactory
+{
+    function make(SenderTypes $type): SenderNotify;
+}
+
 class SenderService
 {
     public function __construct(
